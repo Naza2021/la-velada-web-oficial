ca339dc5a1d3d303cd4119ae2e2b82903b938564
@nazareno.molinari <nazareno.molinari@bitnovo.com>
2024-02-29 16:45:03 +0100

    [feat]: test first worker approach

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,21 @@
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
+import worker from "@astropub/worker"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	adapter: vercel({
-		webAnalytics: { enabled: true },
-	}),
-	output: "hybrid",
-	vite: {
-		build: {
-			cssMinify: "lightningcss",
-		},
-		ssr: {
-			noExternal: ["path-to-regexp"],
-		},
-	},
+  integrations: [tailwind(), worker()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+  output: "hybrid",
+  vite: {
+    build: {
+      cssMinify: "lightningcss",
+    },
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
 })
